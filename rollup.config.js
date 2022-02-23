b990429fa83f46d8fe7984943ca9e91cfb152c3a
andrea morosi <andreamorosi94@gmail.com>
2022-02-23 15:01:53 +0100

    Added rollup, removed webpack

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import scss from 'rollup-plugin-scss'
+import postcss from 'postcss'
+import autoprefixer from 'autoprefixer'
+
+export default {
+  input: './assets/css/main.scss',
+  output: {
+    file: './build/index.js',
+    format: 'esm'
+  },
+  plugins: [
+    scss({
+      processor: () => postcss([autoprefixer()]),
+      outputStyle: 'compressed'
+    })
+  ]
+}
